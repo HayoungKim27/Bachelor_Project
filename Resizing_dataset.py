11902b2227a6dd2baf30353d3f409420c66682db
HayoungKim27 <65437415+HayoungKim27@users.noreply.github.com>
2020-08-06 03:26:41 +0900

    Update Resizing_dataset.py

--- Resizing_dataset.py
+++ Resizing_dataset.py
@@ -8,13 +8,10 @@ def Resizing_dataset():
 	
 	train_path = '/home/haykim/dataset/train_images/1/'
 	train_images = os.listdir(train_path)
-	#test_images = os.listdir('test_images/')
 
 	transform = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor()])
-	#transform까지 전부 하는걸로 진행합시다. 
 
 	for image_name in train_images:
-		#code.interact(local=dict(globals(), **locals()))
 		try:
 			image = io.imread(train_path + image_name)
 
